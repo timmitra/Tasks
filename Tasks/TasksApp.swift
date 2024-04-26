4bb9fb87aea16327e73db7e2adda58269b9c2d76
Tim Mitra <tim@it-guy.com>
2024-04-26 00:58:15 -0400

    Initial Commit

---
+++ Tasks/TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksApp.swift
+//  Tasks
+//
+//  Created by Tim Mitra on 2024-04-26.
+//
+
+import SwiftUI
+
+@main
+struct TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
