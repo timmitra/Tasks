36a63269f3c527ab96e3d5e1aff589bb447f0882
Tim Mitra <tim@it-guy.com>
2024-04-26 01:45:05 -0400

    delete with alert

--- Tasks/TaskView.swift
+++ Tasks/TaskView.swift
@@ -13,35 +13,51 @@ struct TaskView: View {
   @Query<TaskModel>(sort: [SortDescriptor(\.completedDate),
                            SortDescriptor(\.priority)])
   private var tasks: [TaskModel]
+  @State private var deleteTask = false
+  @State private var taskToDelete: TaskModel?
   
     var body: some View {
         List {
           ForEach(tasks) { task in
-            TaskRowView(task: task)
+            HStack {
+              TaskRowView(task: task)
+              Button("Delete Task", systemImage: "trash") {
+                taskToDelete = task
+                deleteTask = true
+              }
+              .labelStyle(.iconOnly)
+              .padding(.leading)
+              .foregroundStyle(.red)   }
           }
-          .onDelete(perform: deleteTask)
+          //.onDelete(perform: deleteTask)
         }
-        .font(.title)
+        .alert("Delete Task?", isPresented: $deleteTask) {
+          Button("Delete", role: .destructive) {
+            modelContext.delete(taskToDelete!)
+            taskToDelete = nil
+          }
+        }
+        //.font(.title)
     }
-  private func deleteTask(indexSet: IndexSet) {
-    for index in indexSet {
-      modelContext.delete(tasks[index])
-    }
-  }
+//  private func deleteTask(indexSet: IndexSet) {
+//    for index in indexSet {
+//      modelContext.delete(tasks[index])
+//    }
+//  }
 }
 
-//#Preview {
-//    TaskView()
-//    .modelContainer(TaskModel.preview)
+#Preview {
+    TaskView()
+    .modelContainer(TaskModel.preview)
+}
+
+//#Preview("Task Incomplete") {
+//  let container = try! ModelContainer(for: TaskModel.self,
+//  configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+//  
+//  return List {
+//    TaskRowView(task: TaskModel(taskName: "Priority 1", priority: 1))
+//    TaskRowView(task: TaskModel(taskName: "Priority 2", priority: 2))
+//    TaskRowView(task: TaskModel(taskName: "Priority 3", priority: 3))
+//  }
 //}
-
-#Preview("Task Incomplete") {
-  let container = try! ModelContainer(for: TaskModel.self,
-  configurations: ModelConfiguration(isStoredInMemoryOnly: true))
-  
-  return List {
-    TaskRowView(task: TaskModel(taskName: "Priority 1", priority: 1))
-    TaskRowView(task: TaskModel(taskName: "Priority 2", priority: 2))
-    TaskRowView(task: TaskModel(taskName: "Priority 3", priority: 3))
-  }
-}
